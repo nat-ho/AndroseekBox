8e469d763b0040996d05dae1b408398b154f8733
Nathaniel Ho <nathaniel_ho1997@hotmail.com>
2021-06-28 14:15:05 +0800

    Add helper function to perform tasks with threading

--- libraries/apkutils.py
+++ libraries/apkutils.py
@@ -1,5 +1,6 @@
 import os
 import re
+from threading import Thread
 
 urlRegEx = "(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+):?\d*)([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?"
 ipRegEx = "\\b(?!(?:10\.|127\.|172\.(?:1[6-9]|2[0-9]|3[0-2])\.|192\.168\.))((?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2(?:[0-4][0-9]|5[0-4])|[0-1]?[0-9]?[0-9])))\\b"
@@ -7,36 +8,74 @@ ipRegEx = "\\b(?!(?:10\.|127\.|172\.(?:1[6-9]|2[0-9]|3[0-2])\.|192\.168\.))((?:(
 urlList = []
 ipList = []
 
-
 def extractUrls(file):
     urls = []
     results = re.findall(urlRegEx, file)
     if (len(results) != 0):
-        for component in results:
-            urlList.append(component[0] + "://" + component[1])
+        for result in results:
+            urlList.append(result[0] + "://" + result[1])
     return urlList
+
 
 def extractIPs(file):
     ips = []
     results = re.findall(ipRegEx, file)
     if (len(results) != 0):
-        for component in results:
-            ipList.append(component[0])
+        for result in results:
+            ipList.append(result)
     return ipList
 
-#Todo: extractGoogleAPIkeys
 
-#Todo: printResults
+def printList(list):
+    counter = 0
+    for item in list:
+        counter += 1
+        print("{}. {}".format(str(counter), item))
 
-#Main function
+
+def printResult():
+    global urlList, ipList
+    urlList = list(set(urlList))
+    ipList = list(set(ipList))
+
+    print("\n---------------------URLs---------------------")
+    if (urlList):
+        print("List of URLs found in the application")
+        printList(urlList)
+    else:
+        print("No URL was found")
+
+    print("\n---------------------IP Addresses---------------------")
+    if (ipList):
+        print("List of IP addresses found in the application")
+        printList(ipList)
+    else:
+        print("No IP address was found")
+
 def startScan(folderPath):
+    hasException = False
     for subdir, dirs, files in os.walk(folderPath):
         for file in files:
-            fullpath = os.path.join(subdir, file)
-            fileobj= open(fullpath, "r", errors = "ignore")
-            filecontent = fileobj.read()
-            fileobj.close()
+            try:
+                fullpath = os.path.join(subdir, file)
+                fileobj= open(fullpath, "r", errors = "ignore")
+                filecontent = fileobj.read()
+                fileobj.close()
+            except Exception as e: 
+                hasException = True
 
-            #Call helper functions
-            urlList = extractUrls(filecontent)
-            ipList = extractIPs(filecontent)
+            try:
+                thread1 = Thread(target = extractUrls, args = (filecontent, ))
+                thread2 = Thread(target = extractIPs, args = (filecontent, ))
+
+                thread1.start()
+                thread2.start()
+
+                thread1.join()
+                thread2.join()
+            except Exception as e:
+                print("Error spawning threads")
+
+    if hasException:
+        print("Some results have have been ommitted due to exceptions")
+    printResult()
--- tool.py
+++ tool.py
@@ -16,7 +16,7 @@ def extract_files(file):
     appName = ntpath.basename(file).split('.')[0]
     folderPath = "OutputAPKs/" + appName
 
-    if not os.path.isdir(folderPath):
+    if (not os.path.isdir(folderPath)):
         #Todo: Add option to re-disassemble if files are already present
 
         print("APK given as input \nUnpacking now")
@@ -29,7 +29,7 @@ def extract_files(file):
     
 
 def print_app_details(xmlDoc):
-    print("""---------------------Application Details---------------------
+    print("""\n---------------------Application Details---------------------
     Name            :{}
     Version Code    :{}
     Version Name    :{}
@@ -39,13 +39,13 @@ def print_app_details(xmlDoc):
     Allow Backup    :{}
     Debuggable      :{}
     """.format(get_attribute(xmlDoc, 'manifest', 'package'), 
-                get_attribute(xmlDoc, 'manifest', 'android:versionCode'),
-                get_attribute(xmlDoc, 'manifest', 'android:versionName'),
-                get_attribute(xmlDoc, 'uses-sdk', 'android:minSdkVersion'),
-                get_attribute(xmlDoc, 'uses-sdk', 'android:targetSdkVersion'),
-                get_attribute(xmlDoc, 'application', 'android:name'),
-                get_attribute(xmlDoc, 'application', 'android:allowBackup'),
-                get_attribute(xmlDoc, 'application', 'android:debuggable')))
+            get_attribute(xmlDoc, 'manifest', 'android:versionCode'),
+            get_attribute(xmlDoc, 'manifest', 'android:versionName'),
+            get_attribute(xmlDoc, 'uses-sdk', 'android:minSdkVersion'),
+            get_attribute(xmlDoc, 'uses-sdk', 'android:targetSdkVersion'),
+            get_attribute(xmlDoc, 'application', 'android:name'),
+            get_attribute(xmlDoc, 'application', 'android:allowBackup'),
+            get_attribute(xmlDoc, 'application', 'android:debuggable')))
 
 def print_exported_components(exportedComponents):
     print("---------------------Exported Components---------------------")
@@ -56,19 +56,19 @@ def print_exported_components(exportedComponents):
 
 try:
 
-    if len(sys.argv[1:]) < 1:
+    if (len(sys.argv[1:]) < 1):
         print_usage()
     
     else:
         inputExtension = sys.argv[1].split('.')[-1]
         appName = sys.argv[1].split('.')[0]
         
-        if inputExtension == 'xml':
+        if (inputExtension == 'xml'):
             xmlDoc = parse_xml(sys.argv[1])
             print_app_details(xmlDoc)
             print_exported_components(xmlDoc)
 
-        elif inputExtension == 'apk':
+        elif (inputExtension == 'apk'):
             outputPath = extract_files(sys.argv[1])
 
             xmlDoc = parse_xml(outputPath + "/resources/AndroidManifest.xml")
@@ -85,4 +85,3 @@ try:
 
 except Exception as e:
     print(e)
-
